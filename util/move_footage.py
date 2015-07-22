d26334b5cc54741579b0be5004b2576d02eb7cf4
Ivan <github-ifermon@sneakemail.com>
2015-07-22 16:52:01 -0400

    added comments / capabiility / debugging / logging

--- util/move_footage.py
+++ util/move_footage.py
@@ -2,29 +2,36 @@
 import sys
 import datetime
 import pysftp
+import re
 from path import path
 
 SOURCE_DIR = '/home/weather/sec_video'
 DEST_DIR = '/mnt/usbstorage/security'
 
 today = datetime.date.today()
-yesterday = today - datetime.timedelta(days=1)
 
 def now():
     return datetime.datetime.now()
 
 
-print("{1} Moving footage from {0}".format(yesterday, now()))
+print("{1} Moving footage not from {0}".format(today, now()))
 
-y_str = "{0}{1:02d}{2:02d}".format(yesterday.year, 
-        yesterday.month, yesterday.day)
+# We search for file names with this pattern to identify files from today
+today_str = "{0}{1:02d}{2:02d}".format(today.year, 
+        today.month, today.day)
+
+# Get the search pattern to identify the day to which a file belongs
+# file names look like this "14-20150702090025.avi"
+# Pattern looks for 4 digits of year, then two digits of month, then two of day
+# pattern is anchored in file name with '-'
+p = re.compile("-(\d\d\d\d)(\d\d)(\d\d)")
 
 files_to_move = []
 files_to_delete = []
 
 src_dir = path(SOURCE_DIR)
 for f in src_dir.files():
-    if not y_str in f.name:
+    if today_str in f.name or 'lastsnap' in f.name:
         print("{1} Not moving {0}".format(f.name, now()))
         continue
     files_to_move.append(f.name)
@@ -33,8 +40,22 @@ for f in src_dir.files():
 with pysftp.Connection('192.168.0.224', username='nas', 
         password='vader370') as sftp:
     with pysftp.cd(SOURCE_DIR):
-        sftp.chdir(DEST_DIR)
         for i in files_to_move:
+            sftp.chdir(DEST_DIR)
+            # First get the date part of the file name
+            m = p.search(i)
+            if m: # we found a matching pattern
+                # Now create dir if doesn't exist and cd to that dir
+                # group(xx) corresponds to the chars matching the ()'s
+                # in the compile statment from above
+                dir_name = "{0}-{1}-{2}".format(m.group(2), m.group(2), 
+                        m.group(1))
+                if not sftp.isdir(dir_name):
+                    print("{0} Directory {1} does not exist, creating".format(
+                            now(), dir_name))
+                    sftp.mkdir(dir_name)
+                sftp.chdir(dir_name)
+
             if not sftp.exists(i):
                 print("{1} Moving {0}".format(i, now()))
                 sftp.put(i, preserve_mtime=True)
