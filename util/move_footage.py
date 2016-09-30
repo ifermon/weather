7c9f0026f5bd5996a8b607537e21dc86d873a2b9
Ivan <github-ifermon@sneakemail.com>
2016-09-30 09:46:08 -0400

    Fixed bug where it was trying to remove non-existing file variable.

--- util/move_footage.py
+++ util/move_footage.py
@@ -27,21 +27,23 @@ today_str = "{0}{1:02d}{2:02d}".format(today.year,
 # pattern is anchored in file name with '-'
 p = re.compile("-(\d\d\d\d)(\d\d)(\d\d)")
 
-files_to_move = []
-files_to_delete = []
+files_to_move = {}
+#files_to_move = []
+#files_to_delete = []
 
 src_dir = path(SOURCE_DIR)
 for f in src_dir.files():
     if today_str in f.name or 'lastsnap' in f.name:
         print("{1} Not moving {0}".format(f.name, now()))
         continue
-    files_to_move.append(f.name)
-    files_to_delete.append(f)
+    #files_to_move.append(f.name)
+    files_to_move[f.name] = f
+    #files_to_delete.append(f)
 
 with pysftp.Connection(const.NAS_IP, username=const.NAS_USER, 
         password=const.NAS_PWD) as sftp:
     with pysftp.cd(SOURCE_DIR):
-        for i in files_to_move:
+        for i in files_to_move.keys():
             sftp.chdir(DEST_DIR)
             if not sftp.exists(i):
                 print("{1} Moving {0}".format(i, now()))
@@ -49,7 +51,9 @@ with pysftp.Connection(const.NAS_IP, username=const.NAS_USER,
             else:
                 print("{1} Not moving {0} - already exists remotely".format(
                     i, now()))
+            print("{1} Removing {0}".format(i, now()))
+            files_to_move[i].remove()
 
-for f in files_to_delete:
-    print("{1} Removing {0}".format(f.name, now()))
-    f.remove()
+#for f in files_to_delete:
+    #print("{1} Removing {0}".format(f.name, now()))
+    #f.remove()
